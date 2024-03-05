731ecd009e312e4062dc0185cc3fc07d4e9324e0
BodiAugustus <tristanettles@gmail.com>
2024-03-04 19:33:13 -0500

    Set up file tree structure. Began creating routes.

---
+++ app/servicesPage/page.jsx
@@ -0,0 +1,9 @@
+export default function servicesPage() {
+  return (
+    <>
+      <div>
+        <h1>Services page</h1>
+      </div>
+    </>
+  );
+}
---
+++ app/subscribe/subscriptions/owned/page.jsx
@@ -0,0 +1,9 @@
+export default function ownedPage() {
+  return (
+    <>
+      <div>
+        <h1>Owned page!</h1>
+      </div>
+    </>
+  );
+}
