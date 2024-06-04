edb19e019983fd870cf2fcf63c6a488fff1fb1b1
BodiAugustus <tristanettles@gmail.com>
2024-06-04 09:46:06 -0400

    finish xl

--- app/components/ui/layout/sections/vaultsPage/Hero.jsx
+++ app/components/ui/layout/sections/vaultsPage/Hero.jsx
@@ -30,7 +30,7 @@ export default function Hero() {
         custom-xs-640-mobile-vaults-hero
         custom-xs-850-mobile-vaults-hero
         custom-xs-860-mobile-vaults-hero  */}
-          <h3 className="text-white font-thin font-sans xs:text-xl xs5:text-2xl md:text-2xl xs:w-[95%] md:w-[60%] lg:w-[40%] text-center custom-xs-410810-mobile-vaults-hero-text">
+          <h3 className="text-white font-thin font-sans xs:text-xl xs5:text-2xl md:text-2xl xs:w-[95%] md:w-[60%] lg:w-[40%] xl:w-[37%] text-center custom-xs-410810-mobile-vaults-hero-text">
             High-yielding, permisionless, auto-compounding vaults with TVL based
             allowances for borrowing against collateral
           </h3>
